545cd1c5f7968901b1127e6191ea02cdb1bc2c79
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-30 12:22:31 -0700

    updated some features

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,22 @@ function displayDayTime() {
   $("#currentDay").html(todayDate);
 }
 setInterval(displayDayTime, 1000);
+
+// click button function to save in the local storage
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  var time = $(this).parent().attr("id");
+  var text = $(this).siblings(".description").val();
+
+  localStorage.setItem(time, text);
+});
+// retrieves items from locale storage
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
