6b6bba39bb23bba5748bff3dd0a805a23313777e
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-29 21:49:41 -0700

    cloned the starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Display current date with time
 function displayDayTime() {
   var todayDate = moment().format("dddd, MMMM Do YYYY h:mm:ss A");
-  $("#current-day").html(todayDate);
+  $("#currentDay").html(todayDate);
 }
 setInterval(displayDayTime, 1000);
