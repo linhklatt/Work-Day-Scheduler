5ed272ec0ba8016a6dcbd51083ada246f8867683
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-29 22:20:40 -0700

    added text box and save button icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Display current date with time
+// Display current date and time
 function displayDayTime() {
   var todayDate = moment().format("dddd, MMMM Do YYYY h:mm:ss A");
   $("#currentDay").html(todayDate);
