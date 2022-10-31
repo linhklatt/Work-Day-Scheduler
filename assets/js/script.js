bc94d8f9858f2c8b4160588cd28c4ed4f188ff42
linhklatt <112294288+linhklatt@users.noreply.github.com>
2022-10-30 20:12:35 -0700

    adjust js code and fixed jquery link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,49 @@
-// Display current date and time
-function displayDayTime() {
-  var todayDate = moment().format("dddd, MMMM Do YYYY h:mm:ss A");
-  $("#currentDay").html(todayDate);
-}
-setInterval(displayDayTime, 1000);
+$(document).ready(function () {
+  // Display current date and time
+  var NowMoment = moment().format("MMMM Do YYYY");
+  var displayDate = document.getElementById("currentDay");
+  displayDate.innerHTML = NowMoment;
+  var currentHour = moment().format("HH");
 
-// click button function to save in the local storage
-$(".saveBtn").click(function (event) {
-  event.preventDefault();
-  var value = $(this).siblings(".time-block").val();
-  var time = $(this).parent().attr("id").split("-")[1];
-  localStorage.setItem(time, value);
+  // clear button to clear local storage and contents
+  $("#clearFieldsBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
+
+  //compare each hour to actual time
+  $(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
+
+  // save data to local storage
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+
+  //retrieves items from local storage
+  $("#hour-09 .time-block").val(localStorage.getItem("09"));
+  $("#hour-10 .time-block").val(localStorage.getItem("10"));
+  $("#hour-11 .time-block").val(localStorage.getItem("11"));
+  $("#hour-12 .time-block").val(localStorage.getItem("12"));
+  $("#hour-13 .time-block").val(localStorage.getItem("13"));
+  $("#hour-14 .time-block").val(localStorage.getItem("14"));
+  $("#hour-15 .time-block").val(localStorage.getItem("15"));
+  $("#hour-16 .time-block").val(localStorage.getItem("16"));
+  $("#hour-17 .time-block").val(localStorage.getItem("17"));
 });
-// retrieves items from locale storage
-$("#hour-9 .time-block").val(localStorage.getItem("9"));
-$("#hour-10 .time-block").val(localStorage.getItem("10"));
-$("#hour-11 .time-block").val(localStorage.getItem("11"));
-$("#hour-12 .time-block").val(localStorage.getItem("12"));
-$("#hour-13 .time-block").val(localStorage.getItem("13"));
-$("#hour-14 .time-block").val(localStorage.getItem("14"));
-$("#hour-15 .time-block").val(localStorage.getItem("15"));
-$("#hour-16 .time-block").val(localStorage.getItem("16"));
-$("#hour-17 .time-block").val(localStorage.getItem("17"));
